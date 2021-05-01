b4a9c1092394a286075989b529ddefc05433544c
Div Loop <80190976+thedivloop@users.noreply.github.com>
2021-05-01 21:40:57 +0700

    added all the logic

--- README.md
+++ README.md
@@ -1,5 +1,158 @@
 # Project #3
 
+# Implementation
+
+# ====== Overall approach ======
+
+## Classes definition
+
+Lifts are to be instances of class **Lift**.  
+_Properties_
+
+- Name
+- Position
+- Doors position
+- is Idle (not working)
+- Is Active (not in emergency state)
+- Next destination stops (array) with number of passengers for every stop. (use a simple array of size 12 and content is the number of passengers)
+- next pick-up stops
+- number of passengers inside
+- isgoing down
+- isgoing up
+
+_Methods_
+
+- Open doors
+- Close doors
+- Go up to max floor X
+- Go down to min floor Y
+- Add floor Z to next destination stops
+- Sent to floor V
+- Set emergency
+- Reset
+- number of entering passengers
+- number of exiting passengers
+
+Passengers are to be instances of class **Passenger**.
+
+_Properties_
+
+- ID
+- starting floor
+- destination floor
+- call lift to go up
+- call lift to go down
+- travel time (from lift call)
+- is Waiting (after lift call)
+- is in the lift A/B
+
+_Methods_
+
+- enter the lift A/B
+- select the destination floor
+- exit the lift
+
+## Constants Definition
+
+- MIN_FLOOR_A = -1
+- MAX_FLOOR_A = 9
+- MIN_FLOOR_B = 0
+- MAX_FLOOR_B = 10
+- TRAVEL_TIME_PER_FLOOR = 1000
+- DOORS_OPENING_TIME = 2000
+- DOORS_CLOSING_TIME = 2000
+- MAXIMUM_LIFT_CAPACITY = 10
+
+## Other variables
+
+- number of waiting passengers per floor (array of )
+
+## Overall algorithm
+
+### Initialization
+
+### Running
+
+After the initialization the algorithm will run on an infinite loop and/or until there is no more passengers to deliver.
+
+When a passenger calls the lift, the system will decide on what lift to select based on the following:
+
+- if 1 lift is idle it gets used for the requesting passenger.
+- if the requesting passenger is on the floor located on the current stops of a lift (next stops)
+- if lift is full capacity it doesnt stop to pick up additional passengers unless there are exiting passengers
+- depending if the lift can access the requested floor
+
+When the floor stops at the passenger floor the doors open and the passenger enters the lift.
+
+When the passenger is inside the lift he presses the destination floor. The next destination stops are incremented accordingly.
+
+The lift continues its journey based on the isgoing down or up properties.
+
+At the new position (floor) it is checked if it is in the list of destination or pick-up stops.
+If it is in the destination stops list it stops and open the doors to let the passengers get out and waiting passengers get in.
+Otherwise if the lift is not full and there are calling passengers it stops. It lets only get in the number of passengers until the lift is full. It then continues its journey until it reaches the last destination stop.
+The lifts go either up or down to last destination/pick-up stop before changing direction.
+Should there be no passengers calling the lift goes into idle at the floor it is and with doors closed.
+
+### Pseudo code
+
+There will be different threads:
+
+- Main
+- 1 per Lift instance
+- 1 per passenger instance
+  Lift threads keep running until user stops the program.
+  Passengers thread stops when passenger arrives at destination floor.
+
+#### Construction/Initialization
+
+Create 2 lifts as new object instances of Lift class
+Initialize the lifts as follows:
+
+- Idle, active, not descending, not going up, min_floor, max_floor
+- Random floor (position)
+  Create X passengers as new object instances of Passenger class
+  Initialize the passengers as follows:
+- Random position
+- Random timeOffset
+- Random destination
+- Randomize if the passenger will press emergency (1/20 chances) and randomize the time offset to press the button after entering the lift. Also randomize after how long the it will be reset.
+
+Get the 2 lifts objects into the Passengers instances.
+
+#### Passengers threads
+
+Started by main at construction
+Wait for the timeOffset
+Start the passenger timer
+Call the lift and get the selected Lift
+Add the floor to the pick-up stops list
+Start the lift thread if the lift is idle.
+When their lift arrives at their floor, enter the lift.
+add the destination floor to the list
+when the lift arrives at the passenger's destination, passenger exits the lift.
+Timer stops and prints the total E2E time in seconds to console.
+
+#### Lifts threads
+
+Start the thread only if a passenger calls and the lift is idle. This means this thread is started from the passenger instance (check if this is possible).
+Then -> while the list of pick-ups and destination stops is not all 0
+
+- Goes to the closest position of the next stops list.
+- Set the direction of the lift to either up or down.
+- Set the Final floor (meaning the floor the lift needs to go before changing direction)
+
+-> While do
+
+- if the position is in final floor toggle the direction
+- close the doors
+- Move to next stop (1sec) (combine in a single method up and down by inputting the direction)
+- At every floor check if it is a position to either pick-up or deliver a passenger. If it is, then broadcast to all passengers the lift position (if there is a list of calling passengers and loaded passengers then broadcast only to that list). Open the doors to let the passengers in and out.
+  Update the pick-ups and destination lists
+- Set the Final floor (meaning the floor the lift needs to go before changing direction)
+
+
+# Pirple specs
 
 ## Details
  
---
+++ index.js
@@ -0,0 +1,312 @@
+
+const MIN_FLOOR_A = -1;
+const MAX_FLOOR_A = 9;
+const MIN_FLOOR_B = 0;
+const MAX_FLOOR_B = 10;
+const TRAVEL_TIME_PER_FLOOR = 1000;
+const DOORS_OPENING_TIME = 2000;
+const DOORS_CLOSING_TIME = 2000;
+const MAXIMUM_LIFT_CAPACITY = 10;
+const TOTAL_PAX = 10;
+
+const paxx = [];
+var lifta;
+var liftb;
+
+var counter = 10;
+
+LiftAllInstances = [];
+
+class Lift {
+    
+    constructor(ID,minF,maxF,position=0) {
+        LiftAllInstances.push(this);
+        this.ID= ID;
+        this.minF =minF;
+        this.maxF =maxF;
+        this.position = position;
+        this.doorIsOpen = false;
+        this.isActive = false;
+        this.nextDestinationStops = [];
+        this.nextPickUpStops = [];
+        this.passengersInside = [];
+        this.direction = "up";
+        this.lastFloor;
+    }
+
+    static getLift(pax) {
+        console.log("Selecting lift...");
+        console.log("The passenger " + pax.id + " is at floor " + pax.startFloor + " and wants to go " + pax.direction + " to floor " + pax.destFloor);
+        var assignedLift;
+        if (pax.destFloor == -1 || pax.startFloor == -1) {
+            assignedLift = lifta;
+        }
+         else if (pax.destFloor == 10 || pax.startFloor == 10) {
+            assignedLift = liftb;
+        } else if (lifta.direction == pax.direction && lifta.position > pax.startFloor && lifta.direction == "down") {
+            assignedLift = lifta;
+        } else if (lifta.direction == pax.direction && lifta.position < pax.startFloor && lifta.direction == "up") {
+            assignedLift = lifta;
+        } else if (liftb.direction == pax.direction && liftb.position > pax.startFloor && liftb.direction == "down") {
+            assignedLift = liftb;
+        } else if (liftb.direction == pax.direction && liftb.position < pax.startFloor && liftb.direction == "up") {
+            assignedLift = liftb;
+        } else if (!liftb.isActive) {
+            assignedLift = liftb;
+        } else {
+            assignedLift = lifta;
+        }
+
+        pax.assignLift(assignedLift);
+        assignedLift.addPickUpFloor(pax.startFloor);
+        
+
+    }
+
+    openDoors(){
+        if (!this.doorIsOpen) {
+            console.log("Lift " + this.ID + " is opening the doors");
+            this.doorIsOpen=true;
+        }
+    }
+
+    closeDoors(){
+        if (this.doorIsOpen) {
+            console.log("Lift " + this.ID + " is closing the doors");
+            this.doorIsOpen=false;
+        }
+    }
+
+    goUptoFloor() {
+        
+        this.lastFloor = Math.max(Math.max.apply(Math, this.nextPickUpStops),Math.max.apply(Math, this.nextDestinationStops));
+        //console.log("Lift is on floor " + this.position + " and last floor is " + this.lastFloor);
+        if (this.position < this.lastFloor && this.position < (this.maxF)) {
+            this.direction = "up";
+            console.log("Lift " + this.ID + " is going " + this.direction);
+            this.position++;
+        } else {
+            console.log("Lift is NOT going " + this.direction + " because it already reached the furthest floor for this journey!");
+        }
+    }
+
+    goDownToFloor() {
+        this.lastFloor = Math.min(Math.min.apply(Math, this.nextPickUpStops), Math.min.apply(Math, this.nextDestinationStops));
+        //console.log("Lift is on floor " + this.position + " and last floor is " + this.lastFloor);
+        if (this.position > this.lastFloor && this.position > (this.minF)) {
+            this.direction = "down";
+            console.log("Lift " + this.ID + " is going " + this.direction);
+            this.position--;
+        } else {
+            console.log("Lift is NOT going " + this.direction + " because it already reached the furthest floor for this journey!");
+        }
+    }
+
+    addDestFloor(destFloor) {
+        if (this.nextDestinationStops .indexOf(destFloor) == -1) {
+            this.nextDestinationStops.push(destFloor);
+        }
+    }
+
+    removeDestFloor() {
+        this.nextDestinationStops.splice(this.nextDestinationStops.indexOf(this.position),1);
+    }
+
+    addPickUpFloor(pickUpFloor) {
+        if (this.nextPickUpStops.indexOf(pickUpFloor) == -1) {
+            this.nextPickUpStops.push(pickUpFloor);
+        }
+    }
+
+    removePickUpFloor() {
+        this.nextPickUpStops.splice(this.nextPickUpStops.indexOf(this.position),1);
+    }
+
+    letPaxIn() {
+        //console.log(pax);
+        if (this.nextPickUpStops.indexOf(this.position) != -1) {
+            for (i=0; i< paxx.length; i++) {
+                if (paxx[i].lift != undefined && paxx[i].lift.ID == this.ID && paxx[i].startFloor == this.position && this.direction == paxx[i].direction)  {
+                    this.openDoors();
+                    console.log("Passenger " + paxx[i].id + " please get into lift " + this.ID);
+                    this.removePickUpFloor();
+                    this.passengersInside.push(paxx[i]);
+                    this.addDestFloor(paxx[i].destFloor);
+                }
+            }
+        }
+    }
+
+    letPaxOut() {
+        
+
+        if (this.nextDestinationStops.indexOf(this.position) != -1) {
+            for (i=0; i< paxx.length; i++) {
+                if (paxx[i].lift != undefined && paxx[i].lift.ID == this.ID && paxx[i].destFloor == this.position)  {
+                    if (!this.doorIsOpen) {
+                        this.openDoors();
+                    }
+                    console.log("Passenger " + paxx[i].id + " please get out of the lift " + this.ID);
+                    paxx[i].endTimer = new Date ();
+                    paxx[i].duration = paxx[i].endTimer.getTime() - paxx[i].startTimer.getTime();
+                    console.log("Passenger " + paxx[i].id + " journey last " + paxx[i].duration/1000 + " seconds");
+                    this.passengersInside.splice(this.passengersInside.indexOf(paxx[i]),1);
+                    this.removeDestFloor();
+                }
+            }
+        }
+        if (this.doorIsOpen) {
+            this.closeDoors();
+        }
+    }
+
+    start() {
+        if (this.checkQueue()) {
+            console.log("Lift " + this.ID + " is starting.");
+            this.isActive = true; 
+        } else {
+            console.log("Lift " + this.ID + " cannot start because job queues are empty.");
+            return;
+        }
+    }
+
+    checkQueue() {
+        //console.log("checking Queue");
+        if (this.nextPickUpStops.length != 0 || this.nextDestinationStops.length !=0) {
+            this.isActive = true;
+            return true;
+        }
+        this.setIdle();
+        return false;
+    }
+
+    setIdle() {
+        if (this.isActive) {
+            console.log("Lift " + this.ID + " is idling.");
+            this.isActive = false;
+        }
+    }
+
+}
+
+
+
+class Pax {
+    constructor(id){
+        while(true) {
+            this.id = id;
+            this.startFloor = Math.floor(Math.random()*(MAX_FLOOR_B+2) -1);
+            this.destFloor = Math.floor(Math.random()*(MAX_FLOOR_B+1));
+            if ((this.destFloor != this.startFloor) && Math.abs(this.startFloor-this.destFloor) < 11) break;
+        }
+        this.direction = "";
+        this.startFloor>this.destFloor? this.direction = "down" : this.direction = "up";
+        this.travelTime = 0;
+        this.status = "queuing";
+        this.lift;
+        this.startTimer;
+        this.endTimer;
+    }
+
+    assignLift(lift) {
+        this.startTimer = new Date();
+        this.lift = lift;
+        
+
+        //this.endTimer = new Date ();
+
+        //this.duration = this.endTimer.getTime() - this.startTimer.getTime();
+        console.log("Passenger " + this.id + " got lift " + this.lift.ID + " assigned.")
+        
+    }
+    getOut() {
+        this.endTimer = new Date ();
+        this.duration = this.endTimer.getTime() - this.startTimer.getTime();
+        console.log("Passenger " + this.id + " journey last " + this.duration + "ms.");
+    }
+}
+
+function initialize() {
+    // create the different objects Pax and Lifts
+
+    console.log("Initializing...")
+    for (i=0;i< TOTAL_PAX; i++) {
+        paxx.push(new Pax(i+1));
+    }
+    lifta = new Lift("A",MIN_FLOOR_A,MAX_FLOOR_A);
+    liftb = new Lift("B",MIN_FLOOR_B,MAX_FLOOR_B);
+
+    for (i=0; i< paxx.length; i++) {
+        var offset = Math.floor(Math.random()*5000);
+        const p = paxx[i];
+        setTimeout((function(p) { 
+            return function() {
+                console.log("Passenger " + p.id + " is pressing on the button " +p.direction);
+                Lift.getLift(p);
+                
+                if (!p.lift.isActive) p.lift.isActive = true;
+            };
+        })(p), offset );
+    
+        // setTimeout((function(p) { 
+        //     return function() {
+        //         p.getOut();
+        //     };
+        // })(p), offset );
+    }
+
+    console.log("Initialized");
+
+    console.log("Lifts are starting...");
+    lifta.start();
+    liftb.start();
+
+
+
+}
+
+// function to be run every second from setInterval
+function runtime() {
+    //console.log("Runtime runs");
+    
+    if (counter==-20) {
+        clearInterval(intervalID);
+        console.log("Interval cleared");
+        return;
+    }
+
+    // Check the queue arrays of the lifts if the 2 arrays of 1 lift are empty the array should idle and the interval cleared
+    for (l of LiftAllInstances) {
+        if (l.checkQueue())  {
+            //console.log(l.nextPickUpStops);
+            //console.log(l.nextDestinationStops);
+            console.log("The lift " + l.ID + " is on floor " + l.position);
+            l.letPaxIn();
+            l.letPaxOut();
+            
+            if (l.position != l.lastFloor) {
+                l.direction == "up" ? l.goUptoFloor() : l.goDownToFloor();
+            } else {
+                if (l.direction == "up") {
+                    l.direction = "down";
+                    l.lastFloor= Math.min(Math.min.apply(Math, this.nextPickUpStops),Math.min.apply(Math, this.nextDestinationStops));
+                } else {
+                    l.direction = "up";
+                    l.lastFloor= Math.max(Math.max.apply(Math, this.nextPickUpStops),Math.max.apply(Math, this.nextDestinationStops));
+                }
+                
+                
+                //console.log("direction reversal");
+            }
+
+        } else {
+            console.log(l.ID + " is already idle.");
+        }
+    }
+
+
+    counter--;
+}
+
+initialize();
+const intervalID = setInterval(runtime,1000);
